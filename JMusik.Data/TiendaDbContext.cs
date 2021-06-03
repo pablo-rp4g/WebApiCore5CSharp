62143cc01c6881da07d15cfb2c235696cc37854e
pablo-rp4g <pablo_rp4g@hotmail.com>
2021-06-03 13:15:01 -0400

    Primer commmit

---
+++ JMusik.Data/TiendaDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using JMusik.Data.Configuracion;
+using JMusik.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace JMusik.Data
+{
+    public partial class TiendaDbContext : DbContext
+    {
+        public TiendaDbContext()
+        {
+        }
+
+        public TiendaDbContext(DbContextOptions<TiendaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<DetalleOrden> DetallesOrden { get; set; }
+        public virtual DbSet<Orden> Ordenes { get; set; }
+        public virtual DbSet<Perfil> Perfiles { get; set; }
+        public virtual DbSet<Producto> Productos { get; set; }
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.ApplyConfiguration(new DetalleOrdenConfiguracion());
+            modelBuilder.ApplyConfiguration(new OrdenConfiguracion());
+            modelBuilder.ApplyConfiguration(new PerfilConfiguracion());
+            modelBuilder.ApplyConfiguration(new ProductoConfiguracion());
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguracion());
+            
+        }
+    }
+}
---
+++ JMusik.WebApi/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using JMusik.Data;
+using JMusik.Data.Contratos;
+using JMusik.Data.Repositorios;
+using JMusik.Models;
+using JMusik.WebApi.Extensions;
+using JMusik.WebApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+
+namespace JMusik.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<TiendaDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("TiendaDb")));
+
+            services.ConfigureDependencies();
+
+            services.ConfigureJwt(Configuration);
+
+            services.ConfigureCors();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddSerilog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+            app.UseCors("CorsPolicy");
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
